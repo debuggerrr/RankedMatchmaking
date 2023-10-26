6664ff07c75a2994e4b0c30e2f8092bf4c8ecc5f
siddhesh.kalgaonkar <siddhesh.kalgaonkar@agilelab.it>
2023-10-26 18:51:34 +0530

    Made the below changes: - Changed the window from keyed to non-keyed window - Changed the ProcessWindowFunction to ProcessAllWindowFunction - Modified the test case

--- core/src/services/extract_record_attributes.py
+++ core/src/services/extract_record_attributes.py
@@ -1,22 +1,21 @@
 from typing import Iterable, Tuple
-from pyflink.datastream import ProcessWindowFunction
+from pyflink.datastream.functions import ProcessAllWindowFunction
 from model.model import UserData
 import ast
 import re
+import datetime
 
 
-class ExtractRecordAttributes(ProcessWindowFunction):
+class ExtractRecordAttributes(ProcessAllWindowFunction):
 
     def __init__(self):
         pass
 
-    def process(self, key: str,
-                context: 'ProcessWindowFunction.Context',
+    def process(self, context: 'ProcessAllWindowFunction.Context',
                 elements: Iterable[Tuple[str, str]]) -> Iterable[Tuple[str, str, str]]:
         """
         This method will group the tuple elements of a specific window in a list
 
-        :param key: The key for which this window is evaluated
         :param context: The context holding window metadata
         :param elements: Input elements
         :return: List of tuples for a specific window frame.
@@ -24,9 +23,13 @@ class ExtractRecordAttributes(ProcessWindowFunction):
         result_list = []
         for element in elements:
             parts = UserData(*ast.literal_eval(str(element)))
-            result = (parts.user,  re.sub(r'\d+', '', parts.rank), str(context.current_processing_time()))
+            result = (parts.user, re.sub(r'\d+', '', parts.rank), str(self.__get_epoch_timestamp()))
             result_list.append(result)
         yield result_list
 
-    def clear(self, context: 'ProcessWindowFunction.Context'):
+    def clear(self, context: 'ProcessAllWindowFunction.Context'):
         pass
+
+    def __get_epoch_timestamp(self):
+        now = datetime.datetime.now()
+        return int(now.timestamp())
--- core/tests/services/test_extract_record_attributes.py
+++ core/tests/services/test_extract_record_attributes.py
@@ -1,4 +1,5 @@
 import unittest
+import datetime
 from unittest.mock import MagicMock, patch
 from services.extract_record_attributes import ExtractRecordAttributes
 
@@ -15,16 +16,16 @@ class TestExtractingRecordAttributes(unittest.TestCase):
         mock_element5 = ('("user5", "silver2")')
         mock_element6 = ('("user6", "silver3")')
 
+        timestamp = str(int(datetime.datetime.now().timestamp()))
         mock_literal_eval.side_effect = lambda s: eval(s)
 
         extractor = ExtractRecordAttributes()
 
-        key = "test_key"
         elements = [mock_element1, mock_element2, mock_element3, mock_element4, mock_element5, mock_element6]
 
-        mock_context.current_processing_time.return_value = 1234567890
+        mock_context.current_processing_time.return_value = timestamp
 
-        results = list(extractor.process(key, mock_context, elements))
+        results = list(extractor.process(mock_context, elements))
 
-        expected_results = [[('user1', 'gold', '1234567890'), ('user2', 'gold', '1234567890'), ('user3', 'gold', '1234567890'), ('user4', 'silver', '1234567890'), ('user5', 'silver', '1234567890'), ('user6', 'silver', '1234567890')]]
+        expected_results = [[('user1', 'gold', timestamp), ('user2', 'gold', timestamp), ('user3', 'gold', timestamp), ('user4', 'silver', timestamp), ('user5', 'silver', timestamp), ('user6', 'silver', timestamp)]]
         self.assertEqual(expected_results, results)
